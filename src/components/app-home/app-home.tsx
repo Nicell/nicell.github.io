35b67b175f81b5b70e62174bd0d5c60ebb954eca
Nick <nick.win999@gmail.com>
2018-12-18 18:11:26 -0600

    Stencil.js initiated

---
+++ src/components/app-home/app-home.tsx
@@ -0,0 +1,19 @@
+import { Component } from '@stencil/core';
+
+@Component({
+  tag: 'app-home',
+  styleUrl: 'app-home.css',
+  shadow: true
+})
+export class AppHome {
+
+  render() {
+    return (
+      <div class='app-home'>
+        <p>
+          🛠Under Construction🛠
+        </p>
+      </div>
+    );
+  }
+}

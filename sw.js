5c56b419e079c091538911fc567d2287b689016c
Nick <nick.win999@gmail.com>
2019-01-01 16:05:58 -0600

    Typo fix and animation element

--- sw.js
+++ sw.js
@@ -25,7 +25,7 @@ self.__precacheManifest = [
   },
   {
     "url": "build/app.js",
-    "revision": "7e71befc5d0ebfd61c9dea7cce636b02"
+    "revision": "e4098bf8e46347bf1d39d22e340e77e9"
   },
   {
     "url": "build/app/69edhwlf.entry.js",
@@ -36,40 +36,48 @@ self.__precacheManifest = [
     "revision": "ebd79d1d62dfc00d891da0660f0bbf91"
   },
   {
-    "url": "build/app/app.2qyv5uxh.js",
-    "revision": "d90cafe6ec6105ebcf22184227f6e4c5"
+    "url": "build/app/app.lmo8ybuk.js",
+    "revision": "94ca3c61b524c671471632acfb3d0a5e"
   },
   {
-    "url": "build/app/app.58pglngs.js",
-    "revision": "e7ef291a5346481acc65e0c77f717ba9"
+    "url": "build/app/app.mjunr3xz.js",
+    "revision": "9a3527477c59026ff52cd42143caa70b"
   },
   {
-    "url": "build/app/ferzjxir.entry.js",
-    "revision": "7c4af38d78abb6e8be676e40739c0509"
+    "url": "build/app/siafoyxv.entry.js",
+    "revision": "4a26853b2c6f635f4208de518e1f69d4"
   },
   {
-    "url": "build/app/ferzjxir.sc.entry.js",
-    "revision": "68221abc578a5f9449f5a97d16df0493"
+    "url": "build/app/siafoyxv.sc.entry.js",
+    "revision": "cd3cce63bfd930b049e9f10b7d595f1e"
   },
   {
-    "url": "build/app/ty1i7kae.entry.js",
-    "revision": "4d8d62eca6845614c2af2b7d68519b1b"
+    "url": "build/app/v55roon5.entry.js",
+    "revision": "bf9e6b3a89f9abd645a1267f930b1fc5"
   },
   {
-    "url": "build/app/ty1i7kae.sc.entry.js",
-    "revision": "c69b9c9901ab017102649c20ded425be"
+    "url": "build/app/v55roon5.sc.entry.js",
+    "revision": "bf9e6b3a89f9abd645a1267f930b1fc5"
   },
   {
-    "url": "build/app/zikh8dut.entry.js",
-    "revision": "ce2907f03bad02f421fcaa7a16690652"
+    "url": "build/app/vktuifz6.entry.js",
+    "revision": "bca4d488567a44b433ed27b10b391b24"
   },
   {
-    "url": "build/app/zikh8dut.sc.entry.js",
-    "revision": "78b83f5a628bd5f0d8a9cc350c0b9894"
+    "url": "build/app/vktuifz6.sc.entry.js",
+    "revision": "3f352e57ddcb331df89e2e8cf537e6c2"
+  },
+  {
+    "url": "build/app/z4t0hsok.entry.js",
+    "revision": "6784f486c3a42f21ae5c9fc1423c0da0"
+  },
+  {
+    "url": "build/app/z4t0hsok.sc.entry.js",
+    "revision": "a8339291f30587eb6f68d1feb3065e07"
   },
   {
     "url": "index.html",
-    "revision": "33ea9cd74d2bed42643f32fd520bd5b9"
+    "revision": "9fbdfc5e35dd1422b4d52a4d647dfde0"
   },
   {
     "url": "manifest.json",

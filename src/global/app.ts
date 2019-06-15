c34f3154aee6579fd4668c31f9174180f83c12ab
Nick <nick.win999@gmail.com>
2019-06-15 11:04:20 -0500

    Add WebFontLoader and fix link contrast in footer.

--- src/global/app.ts
+++ src/global/app.ts
@@ -1,5 +1,12 @@
+import WebFont from 'webfontloader';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faWindow, faServer, faCogs } from '@fortawesome/pro-light-svg-icons';
 import 'intersection-observer';
+
+WebFont.load({
+  google: {
+    families: ['Inconsolata', 'Source Sans Pro']
+  }
+});
 
 library.add(faWindow, faServer, faCogs);

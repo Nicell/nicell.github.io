624a0cdd6af9d33d22302f91a84ad43fa6848ed9
Nick <nick.win999@gmail.com>
2019-06-15 10:25:49 -0500

    Update DevOps skillList and fix build

--- sw.js
+++ sw.js
@@ -11,7 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -20,69 +26,30 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox
  */
 self.__precacheManifest = [
   {
-    "url": "build/app.css",
-    "revision": "18f40f977ba92d31550de903afe4e8f3"
-  },
-  {
-    "url": "build/app.js",
-    "revision": "847c073a829d4c0d45031ae8548fb993"
-  },
-  {
-    "url": "build/app/69edhwlf.entry.js",
-    "revision": "ebd79d1d62dfc00d891da0660f0bbf91"
-  },
-  {
-    "url": "build/app/69edhwlf.sc.entry.js",
-    "revision": "ebd79d1d62dfc00d891da0660f0bbf91"
-  },
-  {
-    "url": "build/app/app.38evhzfz.js",
-    "revision": "dfc12f8591597184a6eaf531ca5cbfb2"
-  },
-  {
-    "url": "build/app/app.3z68nabi.js",
-    "revision": "dffcfaf0d69b8821efeb92cd78c2e187"
-  },
-  {
-    "url": "build/app/ecf2d8zq.entry.js",
-    "revision": "79c1760fc66a70a4fdcd630c72df86b9"
-  },
-  {
-    "url": "build/app/ecf2d8zq.sc.entry.js",
-    "revision": "79c1760fc66a70a4fdcd630c72df86b9"
-  },
-  {
-    "url": "build/app/uzwfrjjv.entry.js",
-    "revision": "b99c1fac98ef88f5b4fabb8166737c4e"
-  },
-  {
-    "url": "build/app/uzwfrjjv.sc.entry.js",
-    "revision": "15a703a817c9d7dfc7d164d3830ccdcb"
-  },
-  {
-    "url": "build/app/vktuifz6.entry.js",
-    "revision": "bca4d488567a44b433ed27b10b391b24"
-  },
-  {
-    "url": "build/app/vktuifz6.sc.entry.js",
-    "revision": "3f352e57ddcb331df89e2e8cf537e6c2"
-  },
-  {
-    "url": "build/app/zmq7c9uo.entry.js",
-    "revision": "64cabb0bd52872ab72383a17a4d9d671"
-  },
-  {
-    "url": "build/app/zmq7c9uo.sc.entry.js",
-    "revision": "aa6728750beba9ffe1b2f9804f711228"
-  },
-  {
     "url": "index.html",
-    "revision": "6821152a2d77a9e4d7a0c2e7c1b90d3a"
+    "revision": "59cd56f959a5412474f965968c402a88"
+  },
+  {
+    "url": "build/p-549b16dd.js"
+  },
+  {
+    "url": "build/p-6325edc4.js"
+  },
+  {
+    "url": "build/p-a0c82e31.js"
+  },
+  {
+    "url": "build/p-f7ddb189.js"
+  },
+  {
+    "url": "build/p-gpqpl3up.css"
+  },
+  {
+    "url": "build/p-s7txuaen.entry.js"
   },
   {
     "url": "manifest.json",
     "revision": "350b0afcb78365eb0d4f287de8c4ff40"
   }
 ].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});

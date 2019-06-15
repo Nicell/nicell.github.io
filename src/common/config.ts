2de2b4fd0484372cdfbce789016fbfcfa20cd8c0
Nick <nick.win999@gmail.com>
2019-06-15 10:25:49 -0500

    Update DevOps skillList and fix build

--- src/common/config.ts
+++ src/common/config.ts
@@ -40,29 +40,32 @@ export const config: configType = {
         'AWS',
         'Vultr',
         'GitHub Pages',
-        'Cloudflare'
+        'Cloudflare',
+        'Docker',
+        'GitLab'
       ]
     }
   ],
   projects: [
     {
       title: 'HLPugs.tf',
-      description: `HLPugs.tf is a captain draft pick up game creator for Team Fortress 2. 
-        It's inspired by a similar project named PugChamp, but it fits into another gamemode PugChamp does not offer. 
-        The current site was built using Vanilla JS, Socket.io, Node.js, and Express. 
+      description: `HLPugs.tf is a captain draft pick up game creator for Team Fortress 2.
+        It's inspired by a similar project named PugChamp, but it fits into another gamemode PugChamp does not offer.
+        The current site was built using Vanilla JS, Socket.io, Node.js, and Express.
         An open source rewrite of the site is currently in progress allowing for more configuration and wider usability.
-        The site has served nearly 5,000 games to over a 1,000 users. 
+        The site has served nearly 5,000 games to over a 1,000 users.
         Today the site is still used every day by TF2 community members in North America and Europe. `,
       image: 'hlpugs',
       direction: 'left'
     },
     {
       title: 'Let\'s Tutor',
-      description: `Let's Tutor is a peer tutoring solution for schools. 
-        It offers a simple way to manage peer tutoring schedules and writing submissions. 
-        Tutors are able to easily view, claim, and edit submissions sent by other students at school. 
-        The site is built using React and Websockets, and it's a Progressive Web App. 
-        Currently this project is being piloted at my school, and I will be looking to expand it's reach next year.`,
+      description: `Let's Tutor is a peer tutoring solution for schools.
+        It offers a simple way to manage peer tutoring schedules and writing submissions.
+        Tutors are able to easily view, claim, and edit submissions sent by other students at school.
+        The site is built using React and Websockets, and it's a Progressive Web App.
+        This project was piloted at my school last year with great success.
+        Currently this project is being upgraded`,
       image: 'letstutor',
       direction: 'right'
     }

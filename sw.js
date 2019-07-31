0201d5f7f305c690449d4551dee7b1774a24c610
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-31 18:17:00 +0000

    Deploy nick.winans.codes to github.com/Nicell/nicell.github.io.git:master

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "index.html",
-    "revision": "35dfe0d398e86297f75782609e80b41b"
+    "revision": "1e434e805dff71b629d38f6d3f788519"
   },
   {
     "url": "build/p-05cd1ccb.js"
@@ -42,10 +42,10 @@ self.__precacheManifest = [
     "url": "build/p-9f78be84.js"
   },
   {
-    "url": "build/p-e6xahnsj.entry.js"
+    "url": "build/p-il4qdixr.css"
   },
   {
-    "url": "build/p-il4qdixr.css"
+    "url": "build/p-tcprbx7l.entry.js"
   },
   {
     "url": "manifest.json",

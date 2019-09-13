c801956adf37f06dd19db71e7e035dd162ce7cbb
Nick <nick.win999@gmail.com>
2019-09-13 11:00:15 -0500

    Improve sentence fluency

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,74 +1,74 @@
-import { configType } from './types';
+import { configType } from "./types";
 
 export const config: configType = {
   skills: [
     {
-      label: 'Front End',
-      icon: 'window',
+      label: "Front End",
+      icon: "window",
       skillList: [
-        'React',
-        'CSS and SASS',
-        'Vanilla JS DOM',
-        'Stencil.js',
-        'WebSockets',
-        'UI Design'
+        "React",
+        "CSS and SASS",
+        "Vanilla JS DOM",
+        "Stencil.js",
+        "WebSockets",
+        "UI Design"
       ]
     },
     {
-      label: 'Back End',
-      icon: 'server',
+      label: "Back End",
+      icon: "server",
       skillList: [
-        'Node.js Ecosystem',
-        'TypeScript',
-        'Golang',
-        'Java',
-        'SQL (MySQL/PG)',
-        'ORM (Hibernate/TypeORM)'
+        "Node.js Ecosystem",
+        "TypeScript",
+        "Golang",
+        "Java",
+        "SQL (MySQL/PG)",
+        "ORM (Hibernate/TypeORM)"
       ]
     },
     {
-      label: 'DevOps',
-      icon: 'cogs',
+      label: "DevOps",
+      icon: "cogs",
       skillList: [
-        'Debian/Ubuntu',
-        'NGINX',
-        'GitHub Pages',
-        'GitLab',
-        'Docker',
-        'Cloudflare'
+        "Debian/Ubuntu",
+        "NGINX",
+        "GitHub Pages",
+        "GitLab",
+        "Docker",
+        "Cloudflare"
       ]
     }
   ],
   projects: [
     {
-      title: 'HLPugs.tf',
+      title: "HLPugs.tf",
       description: `HLPugs.tf is a captain draft pick up game creator for Team Fortress 2.
         It's inspired by a similar project named PugChamp, but it fits into another gamemode PugChamp does not offer.
         The current site was built using Vanilla JS, Socket.io, Node.js, and Express.
         An open source rewrite of the site is currently in progress allowing for more configuration and wider usability.
         The site has served over 5,000 games to over 1,000 users.
         Today the site is still used every day by TF2 community members in North America and Europe.`,
-      image: 'hlpugs',
-      github: 'https://github.com/HLPugs/HLPugs',
-      link: 'https://hlpugs.tf',
-      stack: ['TypeScript', 'React', 'WebSockets', 'Express'],
-      direction: 'left'
+      image: "hlpugs",
+      github: "https://github.com/HLPugs/HLPugs",
+      link: "https://hlpugs.tf",
+      stack: ["TypeScript", "React", "WebSockets", "Express"],
+      direction: "left"
     },
     {
-      title: 'Where to Live',
+      title: "Where to Live",
       description: `Where to Live is a fun side project of parsing data from NOAA using Golang and displaying it using HTML5 Canvas and Stencil.js.
         The outcome of this learning experience is an automatically generated webpage displaying a visual representation of the best places to live based on weather.
-        This project was inspired by Kelly Norton's "Pleasant Places", but extended with the addition of bad days being taken into account, which improved our results.
+        This project was inspired by Kelly Norton's "Pleasant Places", but extended with the addition of bad days being taken into account, which improved the results.
         HTML5 canvas was used instead of DOM or SVG for the increased performance because we render over 3,000 elements.
         The canvas element even has controls that modify the pan and zoom of the map efficiently.`,
-      image: 'wheretolive',
-      github: 'https://github.com/Nicell/where-to-live',
-      link: 'https://nick.winans.codes/where-to-live',
-      stack: ['TypeScript', 'Golang', 'Stencil.js', 'HTML5 Canvas', 'CircleCI'],
-      direction: 'right'
+      image: "wheretolive",
+      github: "https://github.com/Nicell/where-to-live",
+      link: "https://nick.winans.codes/where-to-live",
+      stack: ["TypeScript", "Golang", "Stencil.js", "HTML5 Canvas", "CircleCI"],
+      direction: "right"
     },
     {
-      title: 'Let\'s Tutor',
+      title: "Let's Tutor",
       description: `Let's Tutor is a peer tutoring solution for schools.
         It offers a simple way to manage peer tutoring schedules and writing submissions.
         Tutors are able to easily view, claim, and edit submissions sent by other students at school.
@@ -76,56 +76,56 @@ export const config: configType = {
         This project was piloted at my school last year with great success.
         Currently this project is in the process of an enhancement.
         React Native and a new admin dashboard built with React are on the list of changes.`,
-      image: 'letstutor',
-      link: 'https://letstutor.org',
-      stack: ['JavaScript', 'React', 'Express'],
-      direction: 'left'
+      image: "letstutor",
+      link: "https://letstutor.org",
+      stack: ["JavaScript", "React", "Express"],
+      direction: "left"
     },
     {
-      title: 'CarMon',
+      title: "CarMon",
       description: `CarMon was created for a capstone course in my senior year of high school.
         Our team created a module that sits in your car and measures temperature, CO2 levels, and GPS location.
         This information could then be used to see whether someone is in the car at high temperatures with a location attached.
         The module in the car can connect to the partnering mobile app to sync all of the data via Bluetooth to be visualized.
-        Python was used to write the program that took the raw data from the sensors and recorded it to a SQLite database,
-        and JavaScript was used to transmit the data via Bluetooth and write the mobile app in React Native.`,
-      image: 'carmon',
-      stack: ['JavaScript', 'Python', 'Bluetooth'],
-      direction: 'right'
+        Python was used to write the program that took the raw data from the sensors and recorded it to a SQLite database.
+        Data transmission via Bluetooth and the React native mobile app were written in JavaScript.`,
+      image: "carmon",
+      stack: ["JavaScript", "Python", "Bluetooth"],
+      direction: "right"
     }
   ],
   links: [
     {
       icon: {
-        prefix: 'fab',
-        iconName: 'github'
+        prefix: "fab",
+        iconName: "github"
       },
-      title: 'GitHub',
-      link: 'https://github.com/Nicell'
+      title: "GitHub",
+      link: "https://github.com/Nicell"
     },
     {
       icon: {
-        prefix: 'fas',
-        iconName: 'envelope'
+        prefix: "fas",
+        iconName: "envelope"
       },
-      title: 'Email',
-      link: 'mailto:nick@winans.codes'
+      title: "Email",
+      link: "mailto:nick@winans.codes"
     },
     {
       icon: {
-        prefix: 'fab',
-        iconName: 'linkedin'
+        prefix: "fab",
+        iconName: "linkedin"
       },
-      title: 'LinkedIn',
-      link: 'https://www.linkedin.com/in/nick-winans/'
+      title: "LinkedIn",
+      link: "https://www.linkedin.com/in/nick-winans/"
     },
     {
       icon: {
-        prefix: 'fab',
-        iconName: 'twitch'
+        prefix: "fab",
+        iconName: "twitch"
       },
-      title: 'Twitch',
-      link: 'https://www.twitch.tv/nicell'
+      title: "Twitch",
+      link: "https://www.twitch.tv/nicell"
     }
   ]
-}
+};
